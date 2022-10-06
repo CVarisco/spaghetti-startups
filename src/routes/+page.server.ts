291f37118c403bb09fde5d39d03da65152e7a59f
Christian Varisco <vari.chri1993@gmail.com>
2022-10-06 23:46:20 +0200

    chore: remove console log

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import { supabase } from '$lib/server/supabase';
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
   let { data: startups, error } = await supabase.from('startups').select('*');
-  console.log(startups);
+
   if (startups) {
     return startups;
   }
